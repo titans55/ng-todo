80e006120d76ee3e1b66fc9cd17d3b89ef2f136b
titans55 <kutayaslanfb@hotmail.com>
2020-06-20 16:35:34 +0300

    commit after deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCaYTnnu9NitHscZeYenrsKGHOgtvqEphg',
+    authDomain: 'todo-c764f.firebaseapp.com',
+    databaseURL: 'https://todo-c764f.firebaseio.com',
+    projectId: 'todo-c764f',
+    storageBucket: 'todo-c764f.appspot.com',
+    messagingSenderId: '992840249253',
+    appId: '1:992840249253:web:866f37866cc8949dce424c',
+    measurementId: 'G-RCBY1YNVZG',
+  },
 };
